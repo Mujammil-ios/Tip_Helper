474aa6385359d6efe254c741bdec71421e6dcaf2
Mujammil-ios <mujammilpinjari68507@gmail.com>
2022-04-16 12:40:10 +0500

    added the binding varible

--- app/src/main/java/com/example/hisabkitab/MainActivity.kt
+++ app/src/main/java/com/example/hisabkitab/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.hisabkitab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hisabkitab.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.calculateid.setOnClickListener{
+            calculateTip()
+        
+        }
+
+    }
+
+    fun calculateTip() {
+        TODO("Not yet implemented")
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
