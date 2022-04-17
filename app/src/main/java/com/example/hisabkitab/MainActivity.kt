3c20c521f3aa8c0eea8c40f8ade95d81044cd038
Mujammil-ios <mujammilpinjari68507@gmail.com>
2022-04-17 09:40:00 +0500

    added the logic for app and some documention

--- app/src/main/java/com/example/hisabkitab/MainActivity.kt
+++ app/src/main/java/com/example/hisabkitab/MainActivity.kt
@@ -6,13 +6,21 @@ import com.example.hisabkitab.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+//    To use ViewBinding, you need to declare it as supported in your module-level
+//    build.gradle file under the android section.
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//Writting onclick listner to bind the calculatetip().
 
         binding.calculateid.setOnClickListener{
             calculateTip()
@@ -21,8 +29,44 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun calculateTip() {
-        TODO("Not yet implemented")
+    private fun calculateTip() {
+//        TODO("Not yet implemented")
+
+//In our activity stringInTextField is in double bt some case it can be null to show the
+//restlt to user we need to convert that field to level of input .
+
+        val stringInTextField = binding.billAmount.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+
+//        If input is null show blank result.
+
+
+        if (cost == null) {
+            binding.tipFinal.text = ""
+            return
+        }
+
+
+//When radio button selected to one of them or auto select give the data for calculate percantage.
+
+        val tipPercentage = when (binding.bestChoice.checkedRadioButtonId) {
+            R.id.amzingid -> 0.20
+            R.id.goodid -> 0.15
+            else -> 0.10
+        }
+
+
+//The exuation of app SIMPLE AND EASY.
+
+        var tip = tipPercentage * cost
+        if (binding.switchtip.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+//show the result on textview with currency inctace and formation.
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipFinal.text = getString(R.string.tip_res, formattedTip)
 
 
     }
